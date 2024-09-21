04548e1814c0eee16224dd8dd1a23513baf10c44
Aakarshan17 <164469342+Aakarshan17@users.noreply.github.com>
2024-09-21 01:56:39 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+// Declare and use variables
+
+let studentName = "Aakarshan";
+let studentAge = "27";
+let isEnrolled = true;
+console.log("Student name is", studentName);
+console.log("Student age is", studentAge);
+console.log("Student is enrolled", isEnrolled);  
+
+// Demonstrate Data Types
+
+// String
+let abc = "This is a string";
+console.log(abc);
+
+// Number
+let xyz = 27;
+console.log("This is a number", xyz);
+
+// Boolean
+let isRegistered = true;
+console.log("Student is registered", isRegistered);
+
+// Array
+let subjects = ["Science", "Maths", "Computer", "Geography"];
+console.log("Array:", subjects);
+
+// Object
+let student = {
+    firstName: "Aakarshan",
+    lastName: "Sharma",
+    age: 27
+};
+console.log("Object:", student);
+
+// Using basic operators - arithmetic
+let addition = 5 + 7;
+let subtraction = 7 - 2;
+let multipication = 7 * 5;
+let division = 10 / 2;
+console.log("Addition:", addition);
+console.log("Subtraction:", subtraction);
+console.log("Multipication:", multipication);
+console.log("Division:", division);
+
+// comparision operators
+let x = 3
+let y = 10
+console.log(x>y);
+console.log(x<y);
+console.log(x==y);
+console.log(x!==y);
+
+// logical operators
+
+let t = true;
+let f = false;
+console.log(t && f);
+console.log(t || f);
+console.log(!t);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
